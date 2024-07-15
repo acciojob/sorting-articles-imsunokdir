39badb861cb8a56d63d0d7b179e4ca5668843e77
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-07-15 17:18:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
 function stripArticle(bandName) {
-    return bandName.replace(/^(a |an the )/i, '').trim();
+    return bandName.replace(/^(a |an |the )/i, '').trim();
 }
 
 const sortedBands = bands.sort((a, b) => stripArticle(a).localeCompare(stripArticle(b)));
